d467d98381f555658ade292efb1e8e80acc71ce8
Omorg644 <omorg644@mtroyal.ca>
2025-10-21 13:20:41 -0600

    started module functions, work on algorithm

---
+++ module.py
@@ -0,0 +1,16 @@
+def mean(data:list) -> list:
+    """Return the mean from a list of numerical data"""
+    
+mean()
+
+# def median() -> list:
+#     """Return the median from a list of numerical data"""
+# median()
+
+# def index_air() -> list:
+#     """Make a list of locations and their air quality"""
+# index_air()
+
+# def compare_air() -> list:
+#     """Compare two locations with the best and worst air quality"""
+# compare_air()
