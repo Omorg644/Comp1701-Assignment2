dcbc099a7bb4f4876b73a429d1c94b39c4d1adc2
Omorg644 <omorg644@mtroyal.ca>
2025-10-27 12:46:50 -0600

    List of locations and their air quality

--- algorithm.py
+++ algorithm.py
@@ -1,6 +1,25 @@
 # 
+# Owen Morgan
+# Oct. 22
 # 
 # 
 # 
-# 
-# 
+
+def main()->None:
+    list = []
+    location = input("Location (\"END to stop\"): ")
+    while location != "END":
+        sub_list = []
+        sub_list.append(location)
+        data_june = int(input("Air quality in June: "))
+        data_oct = int(input("Air quality in October: "))
+        sub_list.append(data_june)
+        sub_list.append(data_oct)
+        list.append(sub_list)
+        location = input("Location (\"END to stop\"): ")
+    i = 0
+    while i <len(list):
+        print(f"{i+1}. {list[i][0]} {list[i][1]} {list[i][2]}")
+        i+=1
+    
+main()
