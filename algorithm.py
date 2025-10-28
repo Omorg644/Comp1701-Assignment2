3ac6a8b245712b3b8d166fc243f92fa71c51966e
Omorg644 <omorg644@mtroyal.ca>
2025-10-28 13:19:25 -0600

    air quality data is being assigned to a string

--- algorithm.py
+++ algorithm.py
@@ -2,35 +2,28 @@
 # Owen Morgan
 # Oct. 22
 # 
-# 
-# 
-def mean_calc(air_data:list)->float:
-    i = 0
-    sum = 0
-    while i < len(air_data):
-        sum+=air_data[i][1]
-        i+=1
-    # print(sum)
-    return sum
-    
+
+import module
 
 def main()->None:
-    air_data = []
-    location = input("Location (\"END to stop\"): ")
+    location_data = []
+    location = (input("Location (\"END to stop\"): "))
     while location != "END":
         sub_list = []
         sub_list.append(location)
-        data_june = int(input("Air quality in June: "))
-        data_oct = int(input("Air quality in October: "))
-        sub_list.append(data_june)
-        sub_list.append(data_oct)
-        air_data.append(sub_list)
-        location = input("Location (\"END to stop\"): ")
-    i = 0
-    while i <len(air_data):
-        print(f"{i+1}. {air_data[i][0]} {air_data[i][1]} {air_data[i][2]}")
-        i+=1
-    mean = mean_calc(air_data)
-    print(mean)
+        location_data.append(sub_list)
+        location = (input("Location (\"END to stop\"): "))
+    print(location_data)
+    data_compiled = module.assign_air_quality(location_data)
+    print(data_compiled)
+    # i = 0
+    # while i <len(air_data):
+    #     print(f"{i+1}. {air_data[i][0]} {air_data[i][1]} {air_data[i][2]}")
+    #     i+=1
+    # mean = module.mean_calc(air_data)
+    # print(mean)
+    # median = module.median_calc(air_data)
+    # print(median)
+    
     
 main()
--- module.py
+++ module.py
@@ -1,16 +1,28 @@
-def mean(data:list) -> list:
-    """Return the mean from a list of numerical data"""
+def mean_calc(air_data:list)->int:
+    """Find the mean from a list of air air quality"""
+    i = 0
+    sum = 0
+    while i < len(air_data):
+        sum+=air_data[i][1]
+        i+=1
+    sum = sum//len(air_data)
+    # print(sum)
+    return sum
     
-mean()
+def median_calc(air_data:list)->int:
+    """Find the median from a list of air air quality"""
+    middle = [air_data[len(air_data)//2][1],air_data[len(air_data)//2][2]]
+    # middle.append(air_data[len(air_data)//2][2])
+    return middle
 
-# def median() -> list:
-#     """Return the median from a list of numerical data"""
-# median()
-
-# def index_air() -> list:
-#     """Make a list of locations and their air quality"""
-# index_air()
-
-# def compare_air() -> list:
-#     """Compare two locations with the best and worst air quality"""
-# compare_air()
+def assign_air_quality(location_data:list)->list:
+    """Assign air qaulity data to location names"""
+    i = 0
+    while i<len(location_data):
+        # location_data[i]
+        data_june = int(input(f"{location_data[i][0]}'s air quality in June: "))
+        data_oct = int(input(f"{location_data[i][0]}'s air quality in October: "))
+        location_data[i][0].append(data_june)
+        location_data[i][0].append(data_oct)
+        i+=1
+    return location_data
