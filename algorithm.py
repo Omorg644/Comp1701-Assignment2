f6b62d2797ec4f45862ba53e075c1fec78e4ac65
Omorg644 <omorg644@mtroyal.ca>
2025-11-12 12:49:14 -0700

    hard coded test variables for compared list

--- algorithm.py
+++ algorithm.py
@@ -6,27 +6,29 @@
 import module
 
 def main()->None:
-    location_data = []
-    location = (input("Location (\"END to stop\"): "))
-    while location != "END":
-        sub_list = []
-        sub_list.append(location)
-        location_data.append(sub_list)
-        location = (input("Location (\"END to stop\"): "))
+    # location_data = []
+    # location = (input("Location (\"END to stop\"): "))
+    # while location != "END":
+    #     sub_list = []
+    #     sub_list.append(location)
+    #     location_data.append(sub_list)
+    #     location = (input("Location (\"END to stop\"): "))
     # print(location_data)
-    air_data_compiled = module.assign_air_quality(location_data)
-    air_data_sorted = sorted(air_data_compiled)
-    print(air_data_sorted)
-    # i = 0
-    # while i <len(data_compiled):
-    #     print(f"{i+1}. {data_compiled[i][0]} {data_compiled[i][1]} {data_compiled[i][2]}")
-    #     i+=1
-    mean = module.mean_calc(air_data_compiled)
-    print(mean)
-    median = module.median_calc(air_data_sorted)
-    print(median)
-    sorted_list = module.compare_air_quality(air_data_compiled,location_data)
-    # print(sorted_list)
+    # air_data_compiled = module.assign_air_quality(location_data)
+    # air_data_unsorted = air_data_compiled[1]
+    # print(air_data_unsorted)
+    # air_data_sorted = sorted(air_data_unsorted)
+    # print(air_data_sorted)
+    # mean = module.mean_calc(air_data_unsorted)
+    # print(mean)
+    # median = module.median_calc(air_data_sorted)
+    # print(median)
+
+    # compared_list = module.compare_air_quality(air_data_compiled[0],air_data_sorted)
+    test_location = [['q', 1], ['w', 2], ['e', 3], ['r', 4], ['t', 5], ['y', 6], ['u', 7], ['i', 8], ['o', 9], ['p', 0]]
+    test_sorted = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    compared_list = module.compare_air_quality(test_location,test_sorted)
+    print(compared_list)
     
     
 main()
--- module.py
+++ module.py
@@ -24,15 +24,28 @@ def median_calc(air_data:list)->int:
 def assign_air_quality(location_data:list)->list:
     """Assign air qaulity data to location names"""
     i = 0
-    air_data_compiled = []
+    air_data = []
+    location_air_data = []
+    data_compiled = []
     while i<len(location_data):
         data_oct = int(input(f"{location_data[i][0]}'s air quality in October: "))
-        air_data_compiled.append(data_oct)
+        air_data.append(data_oct)
+        location_air_data.append([location_data[i][0], data_oct])
         i+=1
-    return air_data_compiled
+        data_compiled.append(location_air_data)
+        data_compiled.append(air_data)
+    return data_compiled
 
-def compare_air_quality(air_data:list, location_data:list)->list:
-    sorted = []
-    sorted.append(location_data[air_data[0]])
-    sorted.append(location_data[air_data[len(air_data)-1]])
-    return sorted
+def compare_air_quality(location_data:list, sorted_data:list)->list:
+    print(location_data)
+    print(sorted_data)
+    # sorted = []
+    # i=0
+    # j=-3
+    # while i<len(location_data):
+    #     if location_data[i][1]==sorted_data[j]:
+    #         sorted.append(location_data[i][0])
+    #     i+=1
+    #     if 
+    
+    # return sorted
