07f8d3ac7eaf3040cbb074f4b1676b1ad336fe82
Omorg644 <omorg644@mtroyal.ca>
2025-11-13 13:24:48 -0700

    Find way to save oct and june data

--- algorithm.py
+++ algorithm.py
@@ -5,30 +5,40 @@
 
 import module
 
-def main()->None:
-    # location_data = []
-    # location = (input("Location (\"END to stop\"): "))
-    # while location != "END":
-    #     sub_list = []
-    #     sub_list.append(location)
-    #     location_data.append(sub_list)
-    #     location = (input("Location (\"END to stop\"): "))
-    # print(location_data)
-    # air_data_compiled = module.assign_air_quality(location_data)
-    # air_data_unsorted = air_data_compiled[1]
-    # print(air_data_unsorted)
-    # air_data_sorted = sorted(air_data_unsorted)
-    # print(air_data_sorted)
-    # mean = module.mean_calc(air_data_unsorted)
-    # print(mean)
-    # median = module.median_calc(air_data_sorted)
-    # print(median)
+GOOD_AIR_CUTOFF = 50
 
-    # compared_list = module.compare_air_quality(air_data_compiled[0],air_data_sorted)
-    test_location = [['q', 1], ['w', 2], ['e', 3], ['r', 4], ['t', 5], ['y', 6], ['u', 7], ['i', 8], ['o', 9], ['p', 0]]
-    test_sorted = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-    compared_list = module.compare_air_quality(test_location,test_sorted)
+def main()->None:
+    location_data = []
+    location = (input("Location (\"END to stop\"): "))
+    while location != "END":
+        sub_list = []
+        sub_list.append(location)
+        location_data.append(sub_list)
+        location = (input("Location (\"END to stop\"): "))
+    print(location_data)
+    # month = 
+    # either put all functions in loop with each iteration passing june or oct as param, or have all functions in a seperate moule function with ,onsths as a param
+    air_data_compiled = module.assign_air_quality(location_data, month)
+    air_data_unsorted = air_data_compiled[1]
+    print(air_data_unsorted)
+    air_data_sorted = sorted(air_data_unsorted)
+    print(air_data_sorted)
+
+    mean = module.mean_calc(air_data_unsorted)
+    print(mean)
+
+    median = module.median_calc(air_data_sorted)
+    print(median)
+
+    # test_location = [['q', 1], ['w', 2], ['e', 3], ['r', 4], ['t', 0], ['y', 0], ['u', 0], ['i', 8], ['o', 7], ['p', 9]]
+    # test_sorted = [0, 0, 0, 1, 2, 3, 4, 7, 8, 9]
+    compared_list = module.compare_air_quality(air_data_compiled[0],air_data_sorted)
     print(compared_list)
+
+    # test_sorted = [10, 20, 50, 100, 250, 300, 400, 700, 800, 900]
+    good_percent = module.find_percent(air_data_sorted,GOOD_AIR_CUTOFF)
+    print(good_percent)
     
+
     
 main()
--- module.py
+++ module.py
@@ -21,31 +21,52 @@ def median_calc(air_data:list)->int:
         print(((len(air_data)+1)//2))
     return middle
 
-def assign_air_quality(location_data:list)->list:
+def assign_air_quality(location_data:list, month:str)->list:
     """Assign air qaulity data to location names"""
     i = 0
     air_data = []
     location_air_data = []
     data_compiled = []
     while i<len(location_data):
-        data_oct = int(input(f"{location_data[i][0]}'s air quality in October: "))
-        air_data.append(data_oct)
-        location_air_data.append([location_data[i][0], data_oct])
+        data_month = int(input(f"{location_data[i][0]}'s air quality in {month} "))
+        air_data.append(data_month)
+        location_air_data.append([location_data[i][0], data_month])
         i+=1
         data_compiled.append(location_air_data)
         data_compiled.append(air_data)
     return data_compiled
 
 def compare_air_quality(location_data:list, sorted_data:list)->list:
+    """Return a list of the locations with the worst air quality"""
     print(location_data)
     print(sorted_data)
-    # sorted = []
-    # i=0
-    # j=-3
-    # while i<len(location_data):
-    #     if location_data[i][1]==sorted_data[j]:
-    #         sorted.append(location_data[i][0])
-    #     i+=1
-    #     if 
-    
-    # return sorted
+
+    sorted = []
+    i=0
+    while i<len(location_data):
+        # print("hi hello")
+        j=2 
+        while j >= 1:
+            # print("hello")
+            if location_data[i][1]==sorted_data[-1*j] and not location_data[i][0] in sorted:
+                # print("hi")
+                sorted.append(location_data[i][0])
+            j-=1
+            
+        i+=1
+        # if 
+    print(sorted)
+    return sorted
+
+def find_percent(num_list:list,comparison:int)->float:
+    """Return the percentage of items that are above a specified value"""
+    greater_than = []
+    i = 0
+    while i < len(num_list):
+        if num_list[i] >= comparison:
+            greater_than.append(num_list[i])
+        i+=1
+    percent = (len(greater_than)/len(num_list))*100
+    return percent
+
+# def function_directory()->
